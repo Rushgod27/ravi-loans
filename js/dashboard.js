cc4b95da9c9754946a9b63d2cb88773b9170a164
Rushgod27 <manoj270399@gmail.com>
2026-02-14 22:55:17 +0530

    Update dashboard.js

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,1 +1,23 @@
+// js/dashboard.js
 
+import { auth } from "./firebase.js";
+
+import { 
+  signOut 
+} from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
+
+
+const userData = JSON.parse(localStorage.getItem("raviUser"));
+
+if (!userData) {
+  window.location.href = "login.html";
+} else {
+  document.getElementById("welcome").innerText =
+    "Welcome " + userData.name;
+}
+
+window.logout = async function () {
+  await signOut(auth);
+  localStorage.removeItem("raviUser");
+  window.location.href = "login.html";
+};

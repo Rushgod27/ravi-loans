19e7f29209990e10ae8cbe16aa502d579164aaf5
Rushgod27 <manoj270399@gmail.com>
2026-02-14 22:29:29 +0530

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,1 +1,9 @@
-
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCX4nJv28LRTtCcH3sjj-LUiXzYfxK3SDM",
+  authDomain: "loan-management-system-a5bd8.firebaseapp.com",
+  projectId: "loan-management-system-a5bd8",
+  storageBucket: "loan-management-system-a5bd8.firebasestorage.app",
+  messagingSenderId: "29727312343",
+  appId: "1:29727312343:web:14e58637f47a0994fde392"
+};
